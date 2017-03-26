75f70d9ccf7b2e8ca711b4e99cf691c22298393f
Wei Yang <youi1987@gmail.com>
2017-03-26 16:13:08 +0800

    Check permission & write file.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # android-writefilesample
 Save to files in android &amp; check write permission.
+
+#Referred to  Android Training
+
+* https://developer.android.com/training/basics/data-storage/files.html
+* https://developer.android.com/training/permissions/requesting.html
---
+++ app/src/main/java/com/huawei/mywritefileapp/MainActivity.java
@@ -0,0 +1,147 @@
+package com.huawei.mywritefileapp;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = "MainActivity";
+
+    private final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Assume thisActivity is the current activity
+    }
+
+    public void checkButton_onClick(View v) {
+        checkPermisson();
+    }
+
+    public void writeButton_onClick(View v) {
+        if (isExternalStorageWritable()) {
+            File file = getDownloadStorageFile("Hello.txt");
+            try {
+                PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(file, false)), true);
+                writer.println("Hello, Android!");
+                writer.close();
+            } catch (IOException e) {
+                Log.w(LOG_TAG, "IOException" + e.getMessage());
+            }
+        } else {
+            Log.w(LOG_TAG, "Cannot write file!");
+        }
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state);
+    }
+
+    public File getDownloadStorageFile(String name) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS), name);
+        return file;
+    }
+
+    private void checkPermisson() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // User denied in previous request
+                Log.i(LOG_TAG, "Permission denied, require again.");
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                Toast.makeText(getApplicationContext(),
+                        "The app really need storage permission to write logs.",
+                        Toast.LENGTH_LONG).show();
+
+                // request permission again;
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            } else {
+                // First Run or Always Denied not ask again.
+                Toast.makeText(getApplicationContext(),
+                        "The app need storage permission to write logs.",
+                        Toast.LENGTH_LONG).show();
+                Log.i(LOG_TAG, "require write permission");
+
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+
+
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "已经有存储权限", Toast.LENGTH_LONG).show();
+            Log.i(LOG_TAG, "We already have write permission.");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Log.i(LOG_TAG, "permission granted");
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Log.i(LOG_TAG, "permission denied");
+                }
+                break;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+}
